492acfae1bbd437438fd2c31e528eda849167e2b
scno <scott.d.nobles@gmail.com>
2016-01-24 21:46:46 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Part One: Creating a 2 by 2 matrix 
+## After the 2 by two matrix is created. The function creates a special matrix object. 
 
-## Write a short comment describing this function
+a <- makeCacheMatrix ()
+a$set(matrix(1:4,2,2))
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
+## Part Two. Solving for the inverse of said matrix. 
+## This function calculates the inverse of the matrix. If the inverse of the matrix 
+## has already been calculated then the fucntion will retrive it from the cache instead of calculating it again.  
 
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get ()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
 
+## Run the following line to get the Solution 
 
-## Write a short comment describing this function
+cacheSolve(a)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## The solution itself 
+
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
